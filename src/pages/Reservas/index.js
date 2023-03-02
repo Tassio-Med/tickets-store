f76cac5f14956012da11ec42bac8aca0633c7deb
Tassio-Med <tassiohfmed@gmail.como>
2023-03-02 11:42:14 -0300

    :sparkles: feat: página de reservas estruturada e estilizada inicialmente.

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './reservas.css';
+
+import {MdDelete} from 'react-icons/md';
 
 export default function Reservas() {
   return (
     <div>
-      <h1>RESERVAS</h1>
+      <h1 className='title'>Você solicitou uma reserva</h1>
+
+      <div className='reservas'>
+        <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+        alt="Maceió" 
+        />
+        <strong>Viagem MAceió 7 dias</strong>
+        <span>Quatidade: 2</span>
+        <button
+        type="button"
+        onClick={() => {}}
+        >
+          <MdDelete size={20} color="#191919"/>
+        </button>
+      </div>
+
+      <footer>
+        <button
+        type='button'
+        >
+          Solicitar Reservas
+        </button>
+      </footer>
     </div>
   )
 }
