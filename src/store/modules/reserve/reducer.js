56ab11899ab3bac7d00b8bb61ca215ffcbaecf1d
Tassio-Med <tassiohfmed@gmail.como>
2023-03-02 17:00:03 -0300

    :sparkles: feat: ciado reducer para atualizar quantidade de reservas através dos botões de mais e menos

--- src/store/modules/reserve/reducer.js
+++ src/store/modules/reserve/reducer.js
@@ -1,31 +1,49 @@
-import produce from "immer";
+import produce from 'immer';
 
-export default function reserve(state = [], action) {
-
-  switch (action.type) {
+export default function reserve(state = [], action ){
+  
+  switch(action.type){
     case 'ADD_RESERVE':
       return produce(state, draft => {
         const tripIndex = draft.findIndex(trip => trip.id === action.trip.id);
 
         if(tripIndex >= 0){
           draft[tripIndex].amount +=1;
+          
         }else{
           draft.push({
             ...action.trip,
-            amount: 1,
+            amount:1,
           });
         }
 
       });
-      case 'REMOVE_RESERVE':
-        return produce(state, draft => {
-          const tripIndex = draft.findIndex(trip => trip.id === action.id);
+    case 'REMOVE_RESERVE':
+      return produce(state, draft => {
+        const tripIndex = draft.findIndex(trip => trip.id === action.id);
 
-          if(tripIndex >= 0){
-            draft.splice(tripIndex, 1)
-          }
+        if(tripIndex >= 0){
+          draft.splice(tripIndex, 1);
+        }
 
-        }) 
+      });
+    
+    case 'UPDATE_RESERVE': {
+      if(action.amount <= 0){
+        return state;
+      }
+
+      return produce(state, draft => {
+        const tripIndex = draft.findIndex(trip => trip.id === action.id);
+
+
+        if(tripIndex >= 0){
+          draft[tripIndex].amount = Number(action.amount);
+        }
+
+      });
+
+    }
     default:
       return state;
   }
