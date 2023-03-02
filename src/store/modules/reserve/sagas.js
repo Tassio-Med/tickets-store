99193f840e4ab3a189e83c28756ecec1dacc1ac1
Tassio-Med <tassiohfmed@gmail.como>
2023-03-02 20:03:51 -0300

    :sparkles: feat: reducer apenas adicionando reserva ao array e saga se responsabilizando a verificar o id no state e incrementadno no amount.

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,11 +1,28 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
-import { addReserveSuccess } from './actions';
+import { select, call, put, all, takeLatest } from 'redux-saga/effects';
+import { addReserveSuccess, updateAmountReserve } from './actions';
 import api from '../../../services/api';
 
 function* addToReserve({ id }){
-  const response = yield call(api.get, `trips/${id}`);
+  const tripExists = yield select(
+    state => state.reverse.find(trip => trip.id  === id)
+  );
+  if(tripExists){
+    const amount = tripExists.amount + 1;
 
-  yield put(addReserveSuccess(response.data))
+    yield put(updateAmountReserve(id, amount));
+
+
+  }else{
+    const response = yield call(api.get, `trips/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+    };
+
+    yield put(addReserveSuccess(data))
+  }
+
 }
 
 export default all ([
