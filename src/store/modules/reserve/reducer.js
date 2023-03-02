bbb5879bcdf9b448c4fe65965831eee228e70379
Tassio-Med <tassiohfmed@gmail.como>
2023-03-02 15:16:10 -0300

    :sparkles: feat: quantidade de reservas somando por id

--- src/store/modules/reserve/reducer.js
+++ src/store/modules/reserve/reducer.js
@@ -1,8 +1,22 @@
+import produce from "immer";
+
 export default function reserve(state = [], action) {
-  console.log(state);
+
   switch (action.type) {
     case 'ADD_RESERVE':
-      return [ ...state, action.trip ];
+      return produce(state, draft => {
+        const tripIndex = draft.findIndex(trip => trip.id === action.trip.id);
+
+        if(tripIndex >= 0){
+          draft[tripIndex].amount +=1;
+        }else{
+          draft.push({
+            ...action.trip,
+            amount: 1,
+          });
+        }
+
+      });
     default:
       return state;
   }
