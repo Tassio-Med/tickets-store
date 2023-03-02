44bfa96ae722d5b6c6f576e9e90357eddf0c1346
Tassio-Med <tassiohfmed@gmail.como>
2023-03-02 16:56:22 -0300

    :sparkles: feat: criado dispatch para atualizar input da quantidade de reservas.

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,45 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeReserve, updateAmountReserve } from '../../store/modules/reserve/actions,';
+import { MdDelete, MdAddCircle, MdRemoveCircle } from 'react-icons/md';
 import './reservas.css';
 
-import { useSelector, useDispatch } from 'react-redux';
-import {MdDelete} from 'react-icons/md';
-import { removeReserve } from '../../store/modules/reserve/actions,';
-
 export default function Reservas() {
-  const reserves = useSelector( state => state.reserve );
-  const dispacth = useDispatch();
+ const dispatch = useDispatch();
+ const reserves = useSelector(state => state.reserve);
 
-  function handleRemove(id){
-    dispacth(removeReserve(id));
-  }
+ function handleRemove(id){
+  dispatch(removeReserve(id));
+ }
 
-  return (
-    <div>
-      <h1 className='title'>Você solicitou {reserves.length}</h1>
+ function decrementAmount(trip){
+  dispatch(updateAmountReserve(trip.id, trip.amount - 1));
+ }
 
-      {reserves.map(reserve => (
-        <div className='reservas' key={reserve.id}>
-          <img src={reserve.image}
-          alt={reserve.title} 
-          />
-          <strong>{reserve.title}</strong>
-          <span>Quantidade: {reserve.amount}</span>
-          <button
-          type="button"
-          onClick={() => handleRemove(reserve.id)}
-          >
-            <MdDelete size={20} color="#191919"/>
+ function incrementAmount(trip){
+  dispatch(updateAmountReserve(trip.id, trip.amount + 1));
+ }
+
+ return (
+   <div>
+    <h1 className="title">Voce solicitou {reserves.length} reservas</h1>
+
+    {reserves.map(reserve => (
+      <div className="reservas" key={reserve.id}>
+        <img
+        src={reserve.image}
+        alt={reserve.title}
+        />
+        <strong>{reserve.title}</strong>
+
+        <div id="amount">
+          <button type="button" onClick={()=> decrementAmount(reserve) }>
+            <MdRemoveCircle size={25} color="#252525" />
+          </button>
+          <input type="text" readOnly value={reserve.amount} />
+          <button type="button" onClick={()=> incrementAmount(reserve) }>
+            <MdAddCircle size={25} color="#252525" />
           </button>
         </div>
-      ))}
 
-      <footer>
         <button
-        type='button'
+        type="button"
+        onClick={()=> handleRemove(reserve.id) }
         >
-          Solicitar Reservas
-        </button>
-      </footer>
+        <MdDelete size={20} color="#191919" />
+      </button>
     </div>
-  )
+    ))}
+
+    <footer>
+      <button type="button">Solicitar Reservas</button>
+    </footer>
+
+   </div>
+ );
 }
