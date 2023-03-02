232f406cc3c39ab41399333c360455497e7015e1
Tassio-Med <tassiohfmed@gmail.como>
2023-03-02 17:56:14 -0300

    :sparkles: feat: saga faz requisição no id e dispara action de success

--- src/store/modules/reserve/actions.js
+++ src/store/modules/reserve/actions.js
@@ -1,6 +1,13 @@
-export function addReserve(trip){
+export function addReserveRequest(id){
   return {
-    type: 'ADD_RESERVE',
+    type: 'ADD_RESERVE_REQUEST',
+    id
+  }
+}
+
+export function addReserveSuccess(trip){
+  return {
+    type: 'ADD_RESERVE_SUCCESS',
     trip
   }
 }
